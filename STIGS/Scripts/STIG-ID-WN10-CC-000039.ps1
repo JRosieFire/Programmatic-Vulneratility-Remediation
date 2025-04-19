f3b8b1122bfc4b0d6986be4323c825bcdaec8b40
Jill <89868561+JRosieFire@users.noreply.github.com>
2025-04-19 18:30:09 -0500

    Update STIG-ID-WN10-CC-000039.ps1

--- STIGS/Scripts/STIG-ID-WN10-CC-000039.ps1
+++ STIGS/Scripts/STIG-ID-WN10-CC-000039.ps1
@@ -8,7 +8,7 @@
     GitHub          : 
     Date Created    : 2025-04-19
     Last Modified   : 2025-04-19
-    Version         : 1.0
+    Version         : 2.0
     CVEs            : N/A
     Plugin IDs      : N/A
     STIG-ID         : WN10-CC-000039
@@ -25,99 +25,30 @@
     PS C:\> .\__remediation_template(STIG-ID-WN10-AU-000500).ps1 
 #>
 
-#batfile
-# Define the registry path and value details  
-$regPath = "HKLM:\SOFTWARE\Classes\batfile\shell\runasuser"  
-$valueName = "SuppressionPolicy"  
-$valueType = "DWORD"
-$sizeValue = 4096
-  
+function Set-RegValue {
+	param (
+        $regPath, $valueName, $valueType, $sizeValue
+    )
+	
+	# Ensure the registry path exists; create it if not  
+	if (-not (Test-Path $regPath)) {  
+		New-Item -Path $regPath -Force | Out-Null  
+	}  
 
-# Ensure the registry path exists; create it if not  
-if (-not (Test-Path $regPath)) {  
-    New-Item -Path $regPath -Force | Out-Null  
-}  
+	# Get current Value if exists  
+	$currentSize = (Get-ItemProperty -Path $regPath -Name $valueName -ErrorAction SilentlyContinue).$valueName  
 
-# Get current Value if exists  
-$currentSize = (Get-ItemProperty -Path $regPath -Name $valueName -ErrorAction SilentlyContinue).$valueName  
-
-# Check if current size is less than required and set it if needed  
-if (-not $currentSize -or $currentSize -ne $sizeValue ) {  
-    Set-ItemProperty -Path $regPath -Name $valueName -Value $sizeValue -Type $valueType  
-    Write-Output "Set $valueName to $sizeValue."  
-} else {  
-    Write-Output "valueName is already set to $sizeValue." 
+	# Check if current size is less than required and set it if needed  
+	if (-not $currentSize -or $currentSize -ne $sizeValue ) {  
+		Set-ItemProperty -Path $regPath -Name $valueName -Value $sizeValue -Type $valueType  
+		Write-Output "Set $valueName in $regPath to $sizeValue."  
+	} else {  
+		Write-Output "$valueName in $regPath is already set to $sizeValue." 
 	}
+}
 
 
-#cmdfile
-# Define the registry path and value details  
-$regPath = "HKLM:\SOFTWARE\Classes\cmdfile\shell\runasuser"  
-$valueName = "SuppressionPolicy"  
-$valueType = "DWORD"
-$sizeValue = 4096
-  
-
-# Ensure the registry path exists; create it if not  
-if (-not (Test-Path $regPath)) {  
-    New-Item -Path $regPath -Force | Out-Null  
-}  
-
-# Get current Value if exists  
-$currentSize = (Get-ItemProperty -Path $regPath -Name $valueName -ErrorAction SilentlyContinue).$valueName  
-
-# Check if current size is less than required and set it if needed  
-if (-not $currentSize -or $currentSize -ne $sizeValue ) {  
-    Set-ItemProperty -Path $regPath -Name $valueName -Value $sizeValue -Type $valueType  
-    Write-Output "Set $valueName to $sizeValue."  
-} else {  
-    Write-Output "valueName is already set to $sizeValue." 
-	}
-
-
-
-#exefile
-# Define the registry path and value details  
-$regPath = "HKLM:\SOFTWARE\Classes\exefile\shell\runasuser"  
-$valueName = "SuppressionPolicy"  
-$valueType = "DWORD"
-$sizeValue = 4096
-  
-# Ensure the registry path exists; create it if not  
-if (-not (Test-Path $regPath)) {  
-    New-Item -Path $regPath -Force | Out-Null  
-}  
-
-# Get current Value if exists  
-$currentSize = (Get-ItemProperty -Path $regPath -Name $valueName -ErrorAction SilentlyContinue).$valueName  
-
-# Check if current size is less than required and set it if needed  
-if (-not $currentSize -or $currentSize -ne $sizeValue ) {  
-    Set-ItemProperty -Path $regPath -Name $valueName -Value $sizeValue -Type $valueType  
-    Write-Output "Set $valueName to $sizeValue."  
-} else {  
-    Write-Output "valueName is already set to $sizeValue." 
-	}
-
-#mscfile
-# Define the registry path and value details  
-$regPath = "HKLM:\SOFTWARE\Classes\mscfile\shell\runasuser"  
-$valueName = "SuppressionPolicy"  
-$valueType = "DWORD"
-$sizeValue = 4096
-  
-# Ensure the registry path exists; create it if not  
-if (-not (Test-Path $regPath)) {  
-    New-Item -Path $regPath -Force | Out-Null  
-}  
-
-# Get current Value if exists  
-$currentSize = (Get-ItemProperty -Path $regPath -Name $valueName -ErrorAction SilentlyContinue).$valueName  
-
-# Check if current size is less than required and set it if needed  
-if (-not $currentSize -or $currentSize -ne $sizeValue ) {  
-    Set-ItemProperty -Path $regPath -Name $valueName -Value $sizeValue -Type $valueType  
-    Write-Output "Set $valueName to $sizeValue."  
-} else {  
-    Write-Output "valueName is already set to $sizeValue." 
-	}
+Set-RegValue -regPath "HKLM:\SOFTWARE\Classes\batfile\shell\runasuser" -valueName "SuppressionPolicy" -valueType "DWORD" -sizeValue 4096
+Set-RegValue -regPath "HKLM:\SOFTWARE\Classes\cmdfile\shell\runasuser" -valueName "SuppressionPolicy" -valueType "DWORD" -sizeValue 4096
+Set-RegValue -regPath "HKLM:\SOFTWARE\Classes\exefile\shell\runasuser" -valueName "SuppressionPolicy" -valueType "DWORD" -sizeValue 4096
+Set-RegValue -regPath "HKLM:\SOFTWARE\Classes\mscfile\shell\runasuser" -valueName "SuppressionPolicy" -valueType "DWORD" -sizeValue 4096
