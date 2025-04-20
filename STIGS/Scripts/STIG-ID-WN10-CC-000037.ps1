2d69dea006698fea90c1cfbce12c60735e3dfee6
Jill <89868561+JRosieFire@users.noreply.github.com>
2025-04-19 20:35:21 -0500

    Update STIG-ID-WN10-CC-000037.ps1

--- STIGS/Scripts/STIG-ID-WN10-CC-000037.ps1
+++ STIGS/Scripts/STIG-ID-WN10-CC-000037.ps1
@@ -1,1 +1,52 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures that the Local administrator accounts must have their privileged token filtered to prevent elevated privileges from being used over the network on domain systems.
 
+.NOTES
+    Author          : Jill C
+    LinkedIn        : 
+    GitHub          : 
+    Date Created    : 2025-04-19
+    Last Modified   : 2025-04-19
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-CC-000037
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\__remediation_template(STIG-ID-WN10-AU-000500).ps1 
+#>
+
+# Create a function to implement STIG WN10-SO-000100
+function Set-STIGWN10-CC-000037 {
+	param (
+        $regPath, $valueName, $valueType, $sizeValue
+    )
+	
+	# Ensure the registry path exists; create it if not  
+	if (-not (Test-Path $regPath)) {  
+		New-Item -Path $regPath -Force | Out-Null  
+	}  
+
+	# Get current Value if exists  
+	$currentSize = (Get-ItemProperty -Path $regPath -Name $valueName -ErrorAction SilentlyContinue).$valueName  
+
+	# Check if current size is less than required and set it if needed  
+	if (-not $currentSize -or $currentSize -ne $sizeValue ) {  
+		Set-ItemProperty -Path $regPath -Name $valueName -Value $sizeValue -Type $valueType  
+		Write-Output "Set $valueName in $regPath to $sizeValue."  
+	} else {  
+		Write-Output "$valueName in $regPath is already set to $sizeValue." 
+	}
+}
+
+
+Set-STIGWN10-CC-000037 -regPath "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -valueName "LocalAccountTokenFilterPolicy" -valueType "DWORD" -sizeValue 0
